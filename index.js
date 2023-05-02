5eaaafda211c3b1ec98703626b7db7b6e9af9565
jcatekista <jcatekista@gmail.com>
2023-05-01 19:49:40 -0700

    fixed coffee menu coding

---
+++ index.js
@@ -0,0 +1,9 @@
+//import file
+const coffeeMenu = require("./coffee_data");
+//print array of drinks
+
+//print array of drinks under $5
+//print even priced drinks
+//print total if ordering all drinks
+//print seasonal drinks
+//print seasonal with "with imported beans"
